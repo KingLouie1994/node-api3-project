a155afcb4e76a5df200b6063f1bf2bd0c94f3988
Luis Schekerka <luis@Luiss-MBP.fritz.box>
2020-01-15 14:28:17 +0100

    added logger

--- server.js
+++ server.js
@@ -11,8 +11,16 @@ server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use("/api/users", userRouter);
+server.use("/api/users", logger, userRouter);
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const { method, url } = req;
+  console.log({
+    method,
+    url,
+    timeStamp: Date.now()
+  });
+  next();
+}
 
 module.exports = server;
