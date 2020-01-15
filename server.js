6f5031d44094ab79cfb4d76a202b58ecba370a3b
Luis Schekerka <luis@Luiss-MBP.fritz.box>
2020-01-15 19:30:24 +0100

    changed server.js to receive data from db on the deployed page

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require("express");
 const helmet = require("helmet");
 const userRouter = require("./users/userRouter");
 
+const User = require("./users/userDb");
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  // res.send(`<h2>Let's write some middleware!</h2>`);
+  User.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "There is an error retreiving the users"
+      });
+    });
 });
 
 server.use("/api/users", logger, userRouter);
