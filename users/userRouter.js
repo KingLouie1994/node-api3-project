6579c68e541f5383e3cad237495404438bd58c40
Luis Schekerka <luis@Luiss-MBP.fritz.box>
2020-01-15 15:02:39 +0100

    added validateUser and validateUserID and built endpoint to create new User

--- users/userRouter.js
+++ users/userRouter.js
@@ -10,8 +10,16 @@ const {
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  // do your magic!
+router.post("/", validateUser, (req, res) => {
+  insert(req.body)
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "There is an error creating the new user"
+      });
+    });
 });
 
 router.post("/:id/posts", (req, res) => {
@@ -25,7 +33,7 @@ router.get("/", (req, res) => {
     })
     .catch(error => {
       res.status(500).json({
-        error: "There is an error retreiving the users"
+        message: "There is an error retreiving the users"
       });
     });
 });
@@ -49,11 +57,34 @@ router.put("/:id", (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  getById(req.params.id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(404).json({ message: "User id does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There is an error"
+      });
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (Object.keys(req.body).length && req.body.name) {
+    next();
+  } else if (Object.keys(req.body).length === 0) {
+    res.status(400).json({
+      message: "missing user data"
+    });
+  } else {
+    res.status(400).json({
+      message: "missing required name field"
+    });
+  }
 }
 
 function validatePost(req, res, next) {
